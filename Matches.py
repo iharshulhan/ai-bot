f51b4027c3ed68164dfa9b25c39f419f005418d7
Boris <iguru98@yandex.ru>
2018-01-31 00:09:03 +0300

    Matches bugs fixed

--- Matches.py
+++ Matches.py
@@ -1,9 +1,9 @@
-#In this Puzzle there are 21 Match Sticks.
-#You and Computer will pick up the sticks one by one.
-#Sticks can be picked from 1 to 4.
-#The who, picked up the last stick, is the loser.
+# In this Puzzle there are 21 Match Sticks.
+# You and Computer will pick up the sticks one by one.
+# Sticks can be picked from 1 to 4.
+# The who, picked up the last stick, is the loser.
 
-#http://atozmath.com/Games/21MatchStick.aspx
+# http://atozmath.com/Games/21MatchStick.aspx
 
 '''
     Накидал код для теста сохранения игр пользователей. Закодьте потом нормальный AI
@@ -11,23 +11,38 @@
 '''
 
 max_turn = 4
+begin_amount = 21
+
 
 class Matches:
 
-    def turn(state:int, user_amount:int):
-        #returns tuple (answer, new_state, bot_amount)
-        #There are some cases of game state such as win/lose etc.
-        #In this cases all information will be passed through answer value to user
-        #and new_state and bot_amount will be None
+    def end_game(message):
+        return (message, begin_amount, "")
 
-        new_state = state
-        #check user input
-        if(user_amount <= 0):
+    def turn(state: int, user_amount: int):
+        # returns tuple (answer, new_state, bot_amount)
+        # There are some cases of game state such as win/lose etc.
+        # In this cases all information will be passed through answer value to user
+        # and new_state and bot_amount will be None
+
+        new_state = 0
+        if state is None:
+            new_state = begin_amount
+        else:
+            new_state = state
+        # check user input
+        if user_amount <= 0:
             return ("Cheat <=> miserable live", None, None)
-        if(state - user_amount < 0):
-            return("you done wrong move", None, None)
-        #accept user input
-        new_state = state - user_amount
-        #compute my AI ANN super move
+        if state - user_amount < 0:
+            return ("you done wrong move", None, None)
+        # accept user input
+        new_state = new_state - user_amount
+        if new_state == 0:
+            return Matches.end_game("You lose")
+        # compute my AI ANN super move
         bot_amount = min(max_turn, new_state - 1)
+        if bot_amount <= 0:
+            return Matches.end_game("You win")
+
+        new_state -= bot_amount
         return ("My turn. I'll take: ", new_state, bot_amount)
